2500704584aebbb56d53e770033e272f533927de
Fernanda <femartinsoliveira@hotmail.com>
2019-12-15 02:38:52 -0300

    Rota index

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const app = require('./src/app.js')
 const port = 3000
 
 app.listen(port, () => {
-  console.log(`App rodando na porta ${port}`)
+  console.log(`App está rodando na porta ${port}`)
 })
